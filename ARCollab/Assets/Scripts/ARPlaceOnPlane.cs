23280a66a2f86ecb9f049ce54eac31e523b21be3
Hyemin75 <wjsgpals0705@gmail.com>
2022-10-12 11:49:30 +0900

    [Add] 희영이가 구현한 GameManager과 PlayerPosInfo 추가

--- ARCollab/Assets/Scripts/ARPlaceOnPlane.cs
+++ ARCollab/Assets/Scripts/ARPlaceOnPlane.cs
@@ -10,6 +10,7 @@ public class ARPlaceOnPlane : MonoBehaviour
     public GameObject TigerPrefab;
 
     private GameObject _spawnObject;
+
     private void Update()
     {
         PlaceTigerByTouch();
@@ -32,25 +33,9 @@ public class ARPlaceOnPlane : MonoBehaviour
                 else
                 {
                     _spawnObject.transform.position = hitPose.position;
-                    //_spawnObject.transform.LookAt(GameManager.Instance.PlayerPose);
+                    _spawnObject.transform.LookAt(GameManager.Instance.PlayerPos);
                 }
             }
         }
-    }
-
-    private void UpdateCenterObject()
-    {   //카메라의 센터를 받아오는 함수
-        Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        arRaycaster.Raycast(screenCenter, hits, TrackableType.Planes);
-
-        if(hits.Count > 0)
-        {
-            Pose placementPose = hits[0].pose;
-            TigerPrefab.SetActive(true);
-            TigerPrefab.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
-        }
-
     }
 }
