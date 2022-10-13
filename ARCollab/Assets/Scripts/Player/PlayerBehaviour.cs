5c315e7e8b1584ecaf84995b4fdbe26d2f3eadc4
Hyunsoo Park <smwater130@naver.com>
2022-10-13 09:30:57 +0900

    [Chore]Behaviour 이름 변경

--- ARCollab/Assets/Scripts/Player/PlayerBehaviour.cs
+++ ARCollab/Assets/Scripts/Player/PlayerBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Behaviour : MonoBehaviour
+public class PlayerBehaviour : MonoBehaviour
 {
     private Camera _camera;
 
